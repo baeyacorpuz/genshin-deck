6989d95d504811716c88998a6e28ccf3f0d1e7e1
Bea Aira Lee Corpuz <bcorpuz.cloudpanda@gmail.com>
2022-09-03 11:20:59 +0800

    [ refactor ]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
     </div>
   );
